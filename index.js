a70f708d9e3d47e570e61316095aa2c604313491
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-01-24 15:43:09 +0100

    add new index

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
 const express = require('express');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
-let quotes= {
-    'einstein':'The true sign of intelligence is not knowledge but imagination.',
-    'tesla':'The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane.'
-}
-app.get('/quotes/:name',(request,response)=> {
-    response.status(200).send(quotes[request.params.name]);
-});
-app.get(['/','/index'],function handleRoot(request,response) {
-    response.status(200).send('<h1> Hello in our first REST API. </h1>')
-});
 
-app.post('/',function (request,response) {
-    response.status(200).send('<h1> Hello in our first REST API with Post</h1>')
-});
-
-app.put('/',(request,response) => {
-    response.status(200).send('<h1> Hello in our first REST API with Put</h1>')
-});
-
-app.delete('/',(request,response) => {
-    response.status(200).send('<h1> Hello in our first REST API with Delete</h1>')
-});
-
-app.listen(3000,function success() {
-    console.log('Running on 3000');
-});
+app.listen(port,()=> console.log(`Running on ${port}`));
