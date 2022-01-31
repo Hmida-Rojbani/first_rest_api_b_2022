48f98d0a837c8dda885bf60a1f7bab694003c324
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-01-31 13:45:28 +0100

    student post

--- index.js
+++ index.js
@@ -20,4 +20,15 @@ app.get('/api/students/:id',(req,res)=>{
     res.send(student);
 });
 
+app.use(express.json());
+
+app.post('/api/students',(req,res)=>{
+    let student = {
+        id: students.length+1,
+        name: req.body.name
+    }
+    students.push(student);
+    res.send(student);
+});
+
 app.listen(port,()=> console.log(`Running on ${port}`));
