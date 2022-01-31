a99c600d1f151489cfb355832d69d7b9bd59314a
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-01-31 13:40:23 +0100

    student get

--- index.js
+++ index.js
@@ -3,5 +3,21 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
+let students = [
+    {id :1 , name : 'student1'},
+    {id :2 , name : 'student2'},
+    {id :3 , name : 'student3'}
+]
+
+app.get('/api/students', (req,res)=>{
+    res.send(students);
+});
+
+app.get('/api/students/:id',(req,res)=>{
+    const student = students.find(s=>s.id === parseInt(req.params.id));
+    if (!student)
+        return res.status(404).send('The given id is not found');
+    res.send(student);
+});
 
 app.listen(port,()=> console.log(`Running on ${port}`));
