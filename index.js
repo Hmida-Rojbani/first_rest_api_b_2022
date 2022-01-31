c916a492aa03cffec1d869e11027f5807d4bd87b
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-01-31 15:33:42 +0100

    put et delete

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
-const express = require('express');
-const Joi = require('joi');
+const express = require("express");
+const Joi = require("joi");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 let students = [
-    {id :1 , name : 'student1'},
-    {id :2 , name : 'student2'},
-    {id :3 , name : 'student3'}
-]
+  { id: 1, name: "student1" },
+  { id: 2, name: "student2" },
+  { id: 3, name: "student3" },
+];
 
-app.get('/api/students', (req,res)=>{
-    res.send(students);
+app.get("/api/students", (req, res) => {
+  res.send(students);
 });
 
-app.get('/api/students/:id',(req,res)=>{
-    const student = students.find(s=>s.id === parseInt(req.params.id));
-    if (!student)
-        return res.status(404).send('The given id is not found');
-    res.send(student);
+app.get("/api/students/:id", (req, res) => {
+  const student = students.find((s) => s.id === parseInt(req.params.id));
+  if (!student) return res.status(404).send("The given id is not found");
+  res.send(student);
 });
 
 app.use(express.json());
 
 const schema = Joi.object({
-    name : Joi.string().alphanum().min(3).required()
+  name: Joi.string().alphanum().min(3).required(),
 });
 
-app.post('/api/students',(req,res)=>{
-    // if(!req.body.name )
-    //     return res.status(400).send('Name is required');
-    // if(req.body.name.length < 3)
-    //     return res.status(400).send('Name must at least 3 chars long');
+app.post("/api/students", (req, res) => {
+  // if(!req.body.name )
+  //     return res.status(400).send('Name is required');
+  // if(req.body.name.length < 3)
+  //     return res.status(400).send('Name must at least 3 chars long');
+  let valid_res = schema.validate(req.body);
+  if (valid_res.error)
+    return res.status(400).send(valid_res.error.details[0].message);
+  let student = {
+    id: students.length + 1,
+    name: req.body.name,
+  };
+  students.push(student);
+  res.send(student);
+});
+
+app.put("/api/students/:id", (req, res) => {
+    let student = students.find((s) => s.id === parseInt(req.params.id));
+    if (!student) 
+        return res.status(404).send("The given id is not found");
     let valid_res = schema.validate(req.body);
-    if(valid_res.error)
+    if (valid_res.error)
         return res.status(400).send(valid_res.error.details[0].message);
-    let student = {
-        id: students.length+1,
-        name: req.body.name
-    }
-    students.push(student);
+    student.name=req.body.name;
     res.send(student);
-});
+  });
 
-app.listen(port,()=> console.log(`Running on ${port}`));
+app.delete("/api/students/:id", (req, res) => {
+    const student = students.find((s) => s.id === parseInt(req.params.id));
+    if (!student) return res.status(404).send("The given id is not found");
+    students = students.filter(s => s.id !== parseInt(req.params.id))
+    res.send(student);
+  });
+app.listen(port, () => console.log(`Running on ${port}`));
